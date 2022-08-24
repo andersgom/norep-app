5f31bbfb41e2dce1127d021a6705151ae23afbc2
andersgom <asgomez95@gmail.com>
2022-08-24 18:53:22 +0100

    py files added

---
+++ helper_funcs.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle
